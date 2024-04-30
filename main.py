5cdb3ef6fc574dad0220ae93a3f7267ea6a45204
kosintsev_ag <Andrey.Kosintsev@evraz.com>
2024-04-30 11:15:01 +0700

    baseline

---
+++ main.py
@@ -0,0 +1,41 @@
+# Importing library
+import cv2
+from pyzbar.pyzbar import decode
+
+
+def read_save_barcode(image):
+    # read the image in numpy array using cv2
+    img = cv2.imread(image)
+
+    # Decode the barcode image
+    detectedBarcodes = decode(img)
+
+    # If not detected then print the message
+    if not detectedBarcodes:
+        print("Barcode Not Detected or your barcode is blank/corrupted!")
+    else:
+        # Traverse through all the detected barcodes in image
+        for idx, barcode in enumerate(reversed(detectedBarcodes)):
+
+            # Locate the barcode position in image
+            (x, y, w, h) = barcode.rect
+
+            # Put the rectangle in image using
+            # cv2 to highlight the barcode
+            cv2.rectangle(img, (x - 10, y - 10),
+                          (x + w + 10, y + h + 10),
+                          (255, 0, 0), 2)
+
+            if barcode.data != "":
+                print(idx + 1, str(barcode.data))
+
+            # Display the image
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    # Take the image from user
+    image = "./data/test_image_1.jpg"
+    read_save_barcode(image)
