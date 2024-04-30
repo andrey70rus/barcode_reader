3d4d4d97af1c6eeb6496b09f830cd8b9cf16c580
kosintsev_ag <Andrey.Kosintsev@evraz.com>
2024-04-30 12:28:32 +0700

    baseline with streamlit

--- main.py
+++ main.py
@@ -1,41 +1,71 @@
-# Importing library
 import cv2
+import streamlit as st
+import numpy as np
+
 from pyzbar.pyzbar import decode
+from PIL import Image
+from io import BytesIO
 
 
-def read_save_barcode(image):
-    # read the image in numpy array using cv2
-    img = cv2.imread(image)
+def convert_image(img):
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+    return byte_im
 
-    # Decode the barcode image
-    detectedBarcodes = decode(img)
+
+def generate_output(upload):
+    image = Image.open(upload)
+    image_w_barcodes, detected_barcodes = get_barcodes(image)
+    col1.write("Original Image:")
+    col1.image(image_w_barcodes)
+
+    col2.write("Parsed barcodes:")
+    st.sidebar.markdown("\n")
+    col2.write(detected_barcodes)
+
+
+def get_barcodes(image_loaded):
+    img = np.array(image_loaded)
+    detected_barcodes = decode(image_loaded)
 
     # If not detected then print the message
-    if not detectedBarcodes:
+    if not detected_barcodes:
         print("Barcode Not Detected or your barcode is blank/corrupted!")
     else:
         # Traverse through all the detected barcodes in image
-        for idx, barcode in enumerate(reversed(detectedBarcodes)):
-
+        for idx, barcode in enumerate(reversed(detected_barcodes)):
             # Locate the barcode position in image
             (x, y, w, h) = barcode.rect
 
             # Put the rectangle in image using
             # cv2 to highlight the barcode
-            cv2.rectangle(img, (x - 10, y - 10),
-                          (x + w + 10, y + h + 10),
-                          (255, 0, 0), 2)
+            cv2.rectangle(
+                img, (x - 10, y - 10),
+                (x + w + 10, y + h + 10),
+                (255, 0, 0),
+                2,
+            )
 
             if barcode.data != "":
                 print(idx + 1, str(barcode.data))
 
-            # Display the image
-    cv2.imshow("Image", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    barcodes_values = [i[0] for i in detected_barcodes]
+
+    return img, barcodes_values
 
 
 if __name__ == "__main__":
-    # Take the image from user
-    image = "./data/test_image_1.jpg"
-    read_save_barcode(image)
+    st.set_page_config(layout="wide", page_title="Barcode reader")
+
+    st.sidebar.write("## Upload and download :gear:")
+    MAX_FILE_SIZE = 10 * 1024 * 1024  # 10MB
+
+    col1, col2 = st.columns(2)
+    my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+    if my_upload:
+        if my_upload.size > MAX_FILE_SIZE:
+            st.error("The uploaded file is too large. Please upload an image smaller than 10MB.")
+        else:
+            generate_output(upload=my_upload)
